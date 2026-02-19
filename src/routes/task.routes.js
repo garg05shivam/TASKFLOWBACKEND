676d46758887550ea7e333e2e18787fbfd96ef83
SHIVAM GARG <148767875+garg05shivam@users.noreply.github.com>
2026-02-19 13:09:57 +0530

    feat: add validation middleware to project and task routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
+
 const protect = require("../middlewares/auth.middleware");
+const validate = require("../middlewares/validation.middleware");
+
+const {
+  createTaskValidation,
+  updateTaskValidation,
+  taskQueryValidation,
+} = require("../utils/task.validation");
+
 const {
   create,
   getAll,
@@ -8,9 +17,40 @@ const {
   remove,
 } = require("../controllers/task.controller");
 
-router.post("/", protect, create);
-router.get("/", protect, getAll);
-router.put("/:id", protect, update);
-router.delete("/:id", protect, remove);
+// Create Task
+router.post(
+  "/",
+  protect,
+  createTaskValidation,
+  validate,
+  create
+);
+
+// Get Tasks (with filtering + pagination)
+router.get(
+  "/",
+  protect,
+  taskQueryValidation,
+  validate,
+  getAll
+);
+
+// Update Task
+router.put(
+  "/:id",
+  protect,
+  updateTaskValidation,
+  validate,
+  update
+);
+
+// Delete Task
+router.delete(
+  "/:id",
+  protect,
+  updateTaskValidation,
+  validate,
+  remove
+);
 
 module.exports = router;
